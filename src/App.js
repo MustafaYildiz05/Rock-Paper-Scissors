013547a647152a87dcc8c6d97c23e1eb1b0c88f7
MustafaYildiz05 <MustafaYildiz05@protonmail.com>
2025-05-08 16:35:28 +0300

    I wrote react and necessary js codes in app.js and tested it. the system works fine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Rock from './materials/Rock.png';
+import Paper from './materials/Paper.png';
+import Scissors from './materials/Scissors.png';
+
+var Movement = ['rock', 'paper', 'scissors'];
+var Scores = ['system', 'user', 'draw'];
+
+function randomMovement(userMovement) {
+  var systemIndex = Math.floor(Math.random() * Movement.length);  
+  var systemMovement = Movement[systemIndex];
+  let winner = null;
+
+  if (userMovement === systemMovement) {
+    winner = Scores[2]; // draw 
+  } else if (
+    (userMovement === 'rock' && systemMovement === 'scissors') ||
+    (userMovement === 'paper' && systemMovement === 'rock') ||
+    (userMovement === 'scissors' && systemMovement === 'paper')
+  ) {
+    winner = Scores[1]; // user wins
+  } else {
+    winner = Scores[0]; // system wins
+  }
+
+  alert(winner + systemMovement);
+  return winner;
+}
 
 function App() {
+  const RockClick = () => {
+    randomMovement('rock');
+  };
+  const PaperClick = () => {
+    randomMovement('paper');
+  };
+  const ScissorsClick = () => {
+    randomMovement('scissors');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={Rock} alt="Rock" onClick={RockClick} />
+        <img src={Paper} alt="Paper" onClick={PaperClick}/>
+        <img src={Scissors} alt="Scissors" onClick={ScissorsClick}/>
+      </div>
     </div>
   );
 }
